22ba8ca452a3148f767cf748300f617270a246b5
ksp7518 <60207605+ksp7518@users.noreply.github.com>
2020-02-23 23:06:08 +0900

    최종

--- tcp test2/client.cpp
+++ tcp test2/client.cpp
@@ -1,228 +1,125 @@
-// tcp test2.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
-#include <iostream>
 #include <windows.h>
 #include <TCHAR.H>
-
-#include "framework.h"
-#include "tcp test2.h"
-
-
-
-#define MAX_LOADSTRING 100
-
-// 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
-
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow)
-{
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: 여기에 코드를 입력합니다.
-    WSADATA wsadata;
-    SOCKET	s;
-    SOCKADDR_IN		addr = { 0 };
-
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_TCPTEST2, szWindowClass, MAX_LOADSTRING);
-
-    MyRegisterClass(hInstance);
-
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TCPTEST2));
-
-    MSG msg;
-
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-
-
-
-    WSAStartup(MAKEWORD(2, 2), &wsadata);
-    s = socket(AF_INET, SOCK_STREAM, 0);
-    addr.sin_family = AF_INET;
-    addr.sin_port = 20;
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    if (connect(s, (LPSOCKADDR)&addr, sizeof(addr)) == -1)
-        return 0;
-
-    send(s, "안녕하세요 Server!", 19, 0);
-    
-    closesocket(s);
-    WSACleanup();
-    return 0;
-
-
-
+#define WM_ASYNC WM_USER+2
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, 
+						 WPARAM wParam, LPARAM lParam);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+				   LPSTR lpszCmdLine, int nCmdShow)
+{   
+	HWND 	  hwnd;
+	MSG 	  msg;
+	WNDCLASS  WndClass;   
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;		
+	WndClass.cbClsExtra	= 0;		
+	WndClass.cbWndExtra	= 0;		
+	WndClass.hInstance = hInstance;		
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);	
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	
+	WndClass.lpszMenuName = NULL;		
+	WndClass.lpszClassName = _T("Window Class Name");	
+	RegisterClass(&WndClass);	
+	hwnd = CreateWindow(
+		_T("Window Class Name"),
+		_T("Client Window"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,	
+		CW_USEDEFAULT,	
+		CW_USEDEFAULT,	
+		CW_USEDEFAULT,	
+		NULL,	
+		NULL,	
+		hInstance,	
+		NULL	 
+	);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	while(GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);	
+		DispatchMessage(&msg);	
+	}   
+	return (int)msg.wParam;
 }
 
-
-
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, 
+						 WPARAM wParam, LPARAM lParam)     
 {
-    WNDCLASSEXW wcex;
+	HDC hdc;
+	PAINTSTRUCT ps;
+	static WSADATA wsadata;
+	static SOCKET s;
+	static SOCKADDR_IN addr = { 0 };
+	static TCHAR msg[200];
+	static TCHAR str[100];
+	static char buffer[100];
+	static int count;
+	int msgLen;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TCPTEST2));
-    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_TCPTEST2);
-    wcex.lpszClassName = szWindowClass;
-    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
-
-    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-    if (!hWnd)
-    {
-        return FALSE;
-    }
-
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
-
-    return TRUE;
-}
-
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    static WSADATA wsadata;
-    static SOCKET  s;
-    static SOCKADDR_IN addr = { 0 };
-
-    switch (message)
-    {
-    case WM_CREATE:
-        WSAStartup(MAKEWORD(2, 2), &wsadata);
-        s = socket(AF_INET, SOCK_STREAM, 0);
-        addr.sin_family = AF_INET;
-        addr.sin_port = 20;
-        addr.sin_addr.s_addr = inet_addr("192.168.123.102");
-
-        if (connect(s, (LPSOCKADDR)&addr, sizeof(addr)) == -1)
-            return 0;
-        break;
-    case WM_KEYDOWN:
-        send(s, "안녕 server!!!!", 16, 0);
-        break;
-
-    case WM_COMMAND:
-    {
-        int wmId = LOWORD(wParam);
-        // 메뉴 선택을 구문 분석합니다:
-        switch (wmId)
-        {
-        case IDM_ABOUT:
-            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-            break;
-        case IDM_EXIT:
-            DestroyWindow(hWnd);
-            break;
-        default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
-        }
-    }
-    break;
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
-        // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-        EndPaint(hWnd, &ps);
-    }
-    break;
-    case WM_DESTROY:
-        closesocket(s);
-        WSACleanup();
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	switch (iMsg) 
+	{
+	case WM_CREATE:  
+		WSAStartup(MAKEWORD(2, 2), &wsadata);
+		s = socket(AF_INET, SOCK_STREAM, 0);
+		addr.sin_family = AF_INET;
+		addr.sin_port = 20;
+		addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+		WSAAsyncSelect(s, hwnd, WM_ASYNC, FD_READ);
+		if(connect(s, (LPSOCKADDR)&addr, sizeof(addr)) == -1) 
+			return 0;
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		if (_tcscmp(msg, _T(""))) TextOut(hdc, 0, 30, msg, (int)_tcslen(msg));
+		TextOut(hdc, 0,0, str,_tcslen(str));
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_ASYNC:
+		switch(lParam) 
+		{
+		case FD_READ:
+			msgLen = recv(s, buffer, 100, 0);
+			buffer[msgLen] = NULL;
+#ifdef _UNICODE
+			msgLen = MultiByteToWideChar(CP_ACP, 0, buffer, strlen(buffer), NULL, NULL);
+			MultiByteToWideChar(CP_ACP, 0, buffer, strlen(buffer), msg, msgLen);
+			msg[msgLen] = NULL;
+#else
+			strcpy_s(msg, buffer);
+#endif
+			InvalidateRgn(hwnd, NULL, TRUE);
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_CHAR:
+		if (wParam == VK_RETURN)
+			if (s == INVALID_SOCKET) 
+				return 0;
+			else
+			{
+#ifdef _UNICODE
+				msgLen = WideCharToMultiByte(CP_ACP, 0, str, -1, NULL, 0, NULL, NULL);
+				WideCharToMultiByte(CP_ACP, 0, str, -1, buffer, msgLen, NULL, NULL);
+#else
+				strcpy_s(buffer, str);
+				msgLen = strlen(buffer);
+#endif
+				send(s, (LPSTR )buffer, msgLen+1, 0);
+				count = 0;
+				return 0;
+			}
+		str[count++] = wParam;
+		str[count] = NULL;
+		InvalidateRgn(hwnd, NULL, TRUE);
+		return 0;
+	case WM_DESTROY:
+		closesocket(s);
+		WSACleanup();
+		PostQuitMessage(0);
+		break;
+	} 
+	return DefWindowProc(hwnd, iMsg, wParam, lParam); 
 }
